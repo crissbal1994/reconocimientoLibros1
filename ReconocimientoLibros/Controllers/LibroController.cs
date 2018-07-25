d2b12747beb038e71bff755f4364e43b61849d6d
cristina balcazar <titi.bal@hotmail.es>
2018-07-24 21:19:34 -0500

    cambios

--- ReconocimientoLibros/Controllers/LibroController.cs
+++ ReconocimientoLibros/Controllers/LibroController.cs
@@ -10,14 +10,118 @@ using ReconocimientoLibros.Models;
 
 namespace ReconocimientoLibros.Controllers
 {
-    public class LibroController : Controller
+    public class LibroesController : Controller
     {
-        // GET: Libro
+        private LibreroContext db = new LibreroContext();
+
+        // GET: Libroes
         public ActionResult Index()
+        {
+            return View(db.Libro.ToList());
+        }
+
+        // GET: Libroes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // GET: Libroes/Create
+        public ActionResult Create()
         {
             return View();
         }
-        //private 
+
+        // POST: Libroes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Titulo,Autor,Genero,Anio,Foto,Editorial")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Libro.Add(libro);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(libro);
+        }
+
+        // GET: Libroes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // POST: Libroes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Titulo,Autor,Genero,Anio,Foto,Editorial")] Libro libro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(libro).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(libro);
+        }
+
+        // GET: Libroes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Libro libro = db.Libro.Find(id);
+            if (libro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(libro);
+        }
+
+        // POST: Libroes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Libro libro = db.Libro.Find(id);
+            db.Libro.Remove(libro);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
     }
-    
 }
--- ReconocimientoLibros/Models/Libro.cs
+++ ReconocimientoLibros/Models/Libro.cs
@@ -28,4 +28,37 @@ namespace ReconocimientoLibros.Models
         public DbSet<Libro> Libro { get; set; }
        
     }
+    public class LibreroInitializer : CreateDatabaseIfNotExists<LibreroContext>
+    {
+        protected override void Seed(LibreroContext context)
+        {
+            /*var estantes = new List<Estante>
+            {
+                new Estante{Genero="Novela Romantica"},
+                new Estante{Genero="Ciencia ficcion"}
+            };
+            estantes.ForEach(t => context.estantes.Add(t));
+            context.SaveChanges();
+            var libros = new List<Libro>
+            {
+                new Libro{Titulo="Amor Divina Locura", Genero=1, Autor="Walter Risso", Anio=2001}
+            };
+            libros.ForEach(u => context.libros.Add(u));
+            context.SaveChanges();
+            /*var personas = new List<Persona>
+            {
+                new Persona{Nombre="Paola Serpa", Genero=1, Estudiante="Universitario", Correo="paola.serpa@ucuenca.edu.ec", Edad=20}
+            };
+            personas.ForEach(v => context.personas.Add(v));
+            context.SaveChanges();*/
+        }
+    }
+
+    public class LibreroConfiguration : DbConfiguration
+    {
+        public LibreroConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
 }
